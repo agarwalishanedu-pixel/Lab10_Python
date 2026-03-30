6652ebf3e69a84089df037227eb1fe6e46d70937
Ishan <agarwal.ishanedu@gmail.com>
2026-03-29 22:24:39 -0400

    This create the class and adds constructor

--- Lab10_iagarwal-1.py
+++ Lab10_iagarwal-1.py
@@ -6,7 +6,24 @@ Starter Code: None
 Date: 03/29/2026
 """
 
+#Import
 from pathlib import Path
 import string
+
+class WordAnalyzer:
+    """
+    This class is used to handle, process, and read text files, and count frequencies then produce an output. 
+    """
+
+    def __init__(self, filepath: str):
+        """
+        This function stores the file path and initializes the dictionary.
+        """
+
+        self.__filepath: Path = Path(filepath)
+        self.__frequencies: dict[str, int] = {}
+
+
+
 
 
