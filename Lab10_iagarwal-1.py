2a1faa1a7287c5dbc45493eab9cd6dbc76739511
Ishan <agarwal.ishanedu@gmail.com>
2026-03-29 21:27:38 -0400

    Create file and import

---
+++ Lab10_iagarwal-1.py
@@ -0,0 +1,12 @@
+"""
+Program Name: Lab 10
+My name: Ishan Agarwal
+Purpose: To better understand Object oriented programming, exception handling, path library, string manipulation, and dependency management
+Starter Code: None
+Date: 03/29/2026
+"""
+
+from pathlib import Path
+import string
+
+
